ec44fb672f788f697fcc60229e39d7405801724c
joharbatta <joharbatta@gmail.com>
2020-07-03 01:03:18 +0530

    multer image upload

--- node-auth-passport/app.js
+++ node-auth-passport/app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose=require('mongoose');
 
@@ -55,7 +56,7 @@ app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use('/imageUpload',uploadRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leadership', leaderRouter);
