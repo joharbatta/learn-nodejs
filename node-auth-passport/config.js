5419a7b8ad2f9a16c2aa1c534952e561455b89f7
joharbatta <joharbatta@gmail.com>
2020-07-08 01:02:24 +0530

    outh2 facebook

--- node-auth-passport/config.js
+++ node-auth-passport/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '203661604292031',
-        clientSecret: '5d707114de7125d00a344aceeefa3c4a'
+        clientId: '',
+        clientSecret: ''
     }
 }
